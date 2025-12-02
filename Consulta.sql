a303a7513877568335e6f3c3a2053800682831ba
Kenia <23308060610371@cetis61.edu.mx>
2025-12-02 09:42:33 -0500

    x

--- Consulta.sql
+++ Consulta.sql
@@ -1,2 +1,18 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
+    email VARCHAR(120) UNIQUE,
+    password VARCHAR(255),
+    genero VARCHAR(50),
+    experiencia VARCHAR(100),
+    objetivo VARCHAR(100),
+    alergias VARCHAR(255),
+    intolerancias VARCHAR(255),
+    dieta VARCHAR(100),
+    no_gusta VARCHAR(255)
+);
+
+
 SELECT * FROM usuarios;
 
